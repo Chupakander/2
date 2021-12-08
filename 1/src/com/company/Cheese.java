d44d1cb3cec2edd834b1fbfa84315ad592ed0172
Chupa <matvey.savchenko.0202@mail.ru>
2021-12-08 10:52:06 +0300

    updates

---
+++ 1/src/com/company/Cheese.java
@@ -0,0 +1,18 @@
+package com.company;
+
+
+public class Cheese extends Food {
+
+    public void consume() {
+        System.out.println(this + " съеден");
+    }
+
+    public Cheese(){
+        super("Сыр");
+    }
+
+    @Override
+    public int calculateCalories() {
+        return 300;
+    }
+}
---
+++ 1/src/com/company/Sandwich.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Sandwich extends Food {
+
+    public Sandwich(){
+        super ("Sandwich");
+    }
+
+    public void consume(){
+        System.out.println(this + " съеден");
+    }
+    @Override
+    public int calculateCalories() {
+        return 400;
+    }
+}
